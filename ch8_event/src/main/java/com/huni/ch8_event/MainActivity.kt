8e3baec8bce454b492017751d5938f71824fa020
차지훈 <jihun.cha@tisquare.com>
2021-09-24 16:38:24 +0900

    array test

--- ch8_event/src/main/java/com/huni/ch8_event/MainActivity.kt
+++ ch8_event/src/main/java/com/huni/ch8_event/MainActivity.kt
@@ -2,10 +2,62 @@ package com.huni.ch8_event
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import android.view.KeyEvent
+import android.widget.Toast
+import com.huni.ch8_event.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // Back 키 처리를 위함 (3초 이내에 Back key 두번 눌러야 종료되도록)
+    var initTime = 0L
+    // 스탑워치 pause를 위함
+    var pauseTime = 0L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.startButton.setOnClickListener {
+            binding.chronometer.base = SystemClock.elapsedRealtime() + pauseTime
+            binding.chronometer.start()
+
+            binding.stopButton.isEnabled = true
+            binding.resetButton.isEnabled = true
+            binding.startButton.isEnabled = false
+        }
+
+        binding.stopButton.setOnClickListener {
+            pauseTime = binding.chronometer.base - SystemClock.elapsedRealtime()
+            binding.chronometer.stop()
+
+            binding.stopButton.isEnabled = false
+            binding.resetButton.isEnabled = true
+            binding.startButton.isEnabled = true
+        }
+
+        binding.resetButton.setOnClickListener {
+            pauseTime = 0L
+
+            binding.chronometer.base = SystemClock.elapsedRealtime()
+            binding.chronometer.stop()
+
+            binding.stopButton.isEnabled = false
+            binding.resetButton.isEnabled = false
+            binding.startButton.isEnabled = true
+        }
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode === KeyEvent.KEYCODE_BACK) {
+            if (System.currentTimeMillis() - initTime > 3000) {
+                Toast.makeText(this, "종료하려면 한 번 더 누르세요", Toast.LENGTH_SHORT).show()
+                initTime = System.currentTimeMillis()
+                return true
+            }
+        }
+        return super.onKeyDown(keyCode, event)
     }
 }
