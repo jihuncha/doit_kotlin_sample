faa5c43318914061251367ad3ded8bb66674cff2
차지훈 <>
2021-09-26 16:50:58 +0900

    안드로이드 notification

--- ch10_notification/src/main/java/com/huni/engineer/ch10_notification/DialogTestActivity.kt
+++ ch10_notification/src/main/java/com/huni/engineer/ch10_notification/DialogTestActivity.kt
@@ -1,31 +1,31 @@
 package com.huni.engineer.ch10_notification
 
-import android.app.Activity
-import android.app.AlertDialog
-import android.app.DatePickerDialog
+import android.app.*
 import android.content.DialogInterface
+import android.content.Intent
+import android.graphics.Color
+import android.media.AudioAttributes
 import android.media.RingtoneManager
 import android.net.Uri
-import android.os.Build
-import android.os.Bundle
-import android.os.VibrationEffect
-import android.os.Vibrator
+import android.os.*
 import android.util.Log
 import android.view.View
 import android.widget.DatePicker
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import com.huni.engineer.ch10_notification.databinding.ActivityDialogTestBinding
 
 class DialogTestActivity : AppCompatActivity() {
     val TAG: String = DialogTestActivity::class.java.simpleName
 
-
     var mActivity:Activity? = null
+    var mHandler: Handler? = null
 
     init {
          mActivity = this
+         mHandler = Handler(Looper.getMainLooper())
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -55,6 +55,12 @@ class DialogTestActivity : AppCompatActivity() {
         viewBinding.btSoundVibrateButton.setOnClickListener {
             ringtonAndVibrate()
         }
+
+        viewBinding.btNotificationButton.setOnClickListener(object:View.OnClickListener {
+            override fun onClick(p0: View?) {
+                notificationTest()
+            }
+        })
     }
 
     //api 30 new style - Toast Callback
@@ -142,8 +148,69 @@ class DialogTestActivity : AppCompatActivity() {
         } else {
             vibrator.vibrate(500)
         }
-
     }
+
+    //알림 빌더 생성 -> 알림 객체 생성 -> notify
+    fun notificationTest() {
+        Log.d(TAG, "notificationTest")
+
+        //알림 빌더 생성
+        val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        val builder : NotificationCompat.Builder
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = "one-channel"
+            val channelName = "My Channel One"
+            val channel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_HIGH
+            )
+
+            channel.description = "My Channel One Description"
+            channel.setShowBadge(true)
+
+            //각종 속성 추가
+            val uri: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+            val audioAttributes = AudioAttributes.Builder().
+                    setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION).
+                    setUsage(AudioAttributes.USAGE_ALARM).
+                    build()
+            channel.setSound(uri, audioAttributes)
+            channel.enableLights(true)
+            channel.lightColor = Color.RED
+            channel.enableVibration(true)
+            channel.vibrationPattern = longArrayOf(100, 200, 100, 200)
+
+            manager.createNotificationChannel(channel)
+
+            builder = NotificationCompat.Builder(this, channelId)
+        } else{
+            builder = NotificationCompat.Builder(this)
+        }
+
+        //알림 객체 생성
+        builder.setSmallIcon(android.R.drawable.ic_notification_overlay)
+        builder.setWhen(System.currentTimeMillis())
+        builder.setContentTitle("알림 테스트")
+        builder.setContentText("안녕안녕~")
+
+        //팬딩인텐트
+        val intent: Intent = Intent(this, EmptyActivity::class.java)
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this,
+            10, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        builder.setContentIntent(pendingIntent)
+
+        //알림 - (아이디는 임시로 11로 생성)
+        manager.notify(11, builder.build())
+
+        //취소할 경우 - 5초뒤에
+        mHandler?.postDelayed({
+            Log.d(TAG, "postDelay!!")
+            manager.cancel(11)}, 5000)
+    }
+
+
 
 }
 
--- ch10_notification/src/main/java/com/huni/engineer/ch10_notification/MainActivity.kt
+++ ch10_notification/src/main/java/com/huni/engineer/ch10_notification/MainActivity.kt
@@ -3,6 +3,7 @@ package com.huni.engineer.ch10_notification
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.huni.engineer.ch10_notification.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,12 +12,20 @@ class MainActivity : AppCompatActivity() {
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-//        setContentView(R.layout.activity_main)
 
-        binding.btChap1003.setOnClickListener {
+        binding.btSample.setOnClickListener {
             val intent: Intent = Intent(this, DialogTestActivity::class.java)
 
             startActivity(intent)
         }
+
+        binding.btProject.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(p0: View?) {
+                //TODO this 만으로 안되는 이규..? this.MainActivity 느낌인가?
+                val intent_two = Intent(this@MainActivity, KakaoAlarmSampleActivity::class.java)
+
+                startActivity(intent_two)
+            }
+        })
     }
 }
