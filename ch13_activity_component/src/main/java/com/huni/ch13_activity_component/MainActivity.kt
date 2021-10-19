4e03ddb619afcdb18fd77215e27d51402583c957
차지훈 <jihun.cha@tisquare.com>
2021-10-19 19:01:29 +0900

    13-1 intent

--- ch13_activity_component/src/main/java/com/huni/ch13_activity_component/MainActivity.kt
+++ ch13_activity_component/src/main/java/com/huni/ch13_activity_component/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.huni.ch13_activity_component
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.huni.ch13_activity_component.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvGoNext.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(v: View?) {
+                //action 을 manifest에 지정함 
+//                val intent = Intent("ACTION_EDIT", Uri.parse("http://www.google.com"))
+
+                //좌표값 누르면 선택토록 나온다 -> 암시적 intent
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse("geo:37.7749, 127.4194"))
+                //강제로 설정 가능 -> 구글맵으로
+//                intent.setPackage("com.google.android.apps.maps")
+                startActivity(intent)
+            }
+        })
+
+
+
     }
 }
---
+++ ch13_activity_component/src/main/java/com/huni/ch13_activity_component/SecondActivity.kt
@@ -0,0 +1,11 @@
+package com.huni.ch13_activity_component
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
