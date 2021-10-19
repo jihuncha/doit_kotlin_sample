0519957fb352c6d299e4bb826a0bb988450c4fbf
차지훈 <jihun.cha@tisquare.com>
2021-10-19 17:17:15 +0900

    ch13 module 추가

---
+++ ch13_activity_component/src/main/java/com/huni/ch13_activity_component/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.huni.ch13_activity_component
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
