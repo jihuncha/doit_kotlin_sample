8c58fb13449893c89705cfdac5915d889456c92b
차지훈 <jihun.cha@tisquare.com>
2021-11-02 18:26:44 +0900

    softkeyboard동작

--- ch13_activity_component/src/main/java/com/huni/ch13_activity_component/BundleTestActivity.kt
+++ ch13_activity_component/src/main/java/com/huni/ch13_activity_component/BundleTestActivity.kt
@@ -1,15 +1,18 @@
 package com.huni.ch13_activity_component
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.util.Log
+import android.view.inputmethod.InputMethodManager
 import com.huni.ch13_activity_component.databinding.ActivityBundleTestBinding
 
+// 번들 test 및 softKeyboard 제어 관련
 class BundleTestActivity : AppCompatActivity() {
     val TAG:String = BundleTestActivity::class.java.simpleName
 
     private lateinit var binding: ActivityBundleTestBinding
+    private lateinit var manager: InputMethodManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d(TAG, "onCreate")
@@ -18,6 +21,24 @@ class BundleTestActivity : AppCompatActivity() {
 
         binding = ActivityBundleTestBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initView()
+    }
+
+    private fun initView() {
+        manager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+        //토글
+//        binding.btToggleFocus.setOnClickListener{
+//            manager.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
+//        }
+
+        //show
+        binding.btToggleFocus.setOnClickListener{
+            binding.etEditText.requestFocus()
+            manager.showSoftInput(binding.etEditText, InputMethodManager.SHOW_IMPLICIT)
+        }
+
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
