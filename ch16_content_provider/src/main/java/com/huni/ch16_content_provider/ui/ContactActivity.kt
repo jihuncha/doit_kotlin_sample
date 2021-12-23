d1da90c28319274eae99d099e0069795646bf55b
차지훈 <jihun5000@naver.com>
2021-12-23 15:02:20 +0900

    연락처 정보 가져오기..android11 권한부여주의하자

--- ch16_content_provider/src/main/java/com/huni/ch16_content_provider/ui/ContactActivity.kt
+++ ch16_content_provider/src/main/java/com/huni/ch16_content_provider/ui/ContactActivity.kt
@@ -13,19 +13,66 @@ import com.huni.ch16_content_provider.databinding.ActivityContactBinding
 
 class ContactActivity : AppCompatActivity() {
     companion object {
-        val TAG : String = ContactActivity::class.java.simpleName
+        val TAG: String = ContactActivity::class.java.simpleName
     }
 
     private lateinit var binding: ActivityContactBinding
 
-    private val getActivityResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-        if (it.resultCode == Activity.RESULT_OK) {
-            val result: String? = it.data?.dataString
-            //url 형태로 데이터가 내려온다
-            //이를 식별값 조건으로 주로속 앱에 필요한 데이터를 구체적으로 요청해야한다!!
-            Log.d(TAG, "result - $result")
+
+    //    private val getActivityResult =
+//        registerForActivityResult(ActivityResultContracts.GetContent()) { result ->
+//                val cursor = contentResolver.query(
+//                    result,
+//                    arrayOf<String>(
+//                        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+//                        ContactsContract.CommonDataKinds.Phone.NUMBER
+//                    ),
+//                    null,
+//                    null,
+//                    null
+//                )
+//
+//            Log.d(TAG, "cursor size - ${cursor?.count}")
+//
+//            if (cursor!!.moveToFirst()) {
+//                val name = cursor?.getString(0)
+//                val phone = cursor?.getString(1)
+//
+//                Log.d(TAG, "name - $name and phone - $phone")
+//            }
+
+    private val getActivityResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { activityResult ->
+            if (activityResult.resultCode == Activity.RESULT_OK) {
+//            val result: String? = activityResult.data?.dataString
+                //url 형태로 데이터가 내려온다
+                //이를 식별값 조건으로 주소록 앱에 필요한 데이터를 구체적으로 요청해야한다!!
+            Log.d(TAG, "result - ${activityResult.data!!.data}")
+
+                val cursor = contentResolver.query(
+                    //intent의 data
+                    activityResult.data!!.data!!,
+                    arrayOf<String>(
+                        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                        ContactsContract.CommonDataKinds.Phone.NUMBER
+                    ),
+                    null,
+                    null,
+                    null
+                )
+
+                //TODO 왜 특정단말(android 11)에서 계속 0이 나오지?? (android 9는 정상)
+                // 권한 허용을 해줘야함..!! .....
+                Log.d(TAG, "cursor size - ${cursor?.count}")
+
+                if (cursor!!.moveToFirst()) {
+                    val name = cursor?.getString(0)
+                    val phone = cursor?.getString(1)
+
+                    Log.d(TAG, "name - $name and phone - $phone")
+                }
+            }
         }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,11 +81,13 @@ class ContactActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btShowContact.setOnClickListener {
-            val intent = Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI)
+            //전화번호가 있는 사람
+            val intent =
+                Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI)
             getActivityResult.launch(intent)
 
+//            getActivityResult.launch(ActivityResultContracts.PickContact().createIntent(baseContext, ContactsContract.CommonDataKinds.Phone.CONTENT_URI))
         }
-
     }
 
 
