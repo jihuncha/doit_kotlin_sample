3cb1037d31826995646dc1bbf8dea026fdd127e5
Jihun <jihun5000@naver.com>
2022-02-12 17:12:42 +0900

    인증진행중

---
+++ ch20_firebase/src/main/java/com/huni/ch20_firebase/MyApplication.kt
@@ -0,0 +1,34 @@
+package com.huni.ch20_firebase
+
+import androidx.multidex.MultiDexApplication
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class MyApplication : MultiDexApplication() {
+    companion object {
+        lateinit var auth: FirebaseAuth
+
+        var email: String? = null
+
+        fun checkAuth(): Boolean {
+            val currentUser = auth.currentUser
+            return currentUser?.let {
+                email = currentUser.email
+                if (currentUser.isEmailVerified) {
+                    true
+                } else {
+                    false
+                }
+                //TODO 왜 이렇게했지? ?: false 면 되지 않나..?
+            } ?: let {
+                false
+            }
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        auth = Firebase.auth
+    }
+}
